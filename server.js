59025356bf69361bcf604946b84882509c205b31
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2025-02-22 03:08:33 +0000

    server.js updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 const dataFilePath = path.join(__dirname, 'data.json');
 
 // Middleware to parse JSON
